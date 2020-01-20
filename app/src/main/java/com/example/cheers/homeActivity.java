e513556bfedf40ed7b4536afa5ddd1d9abdb97a6
Alma Ayaquica <yogo0607@gmail.com>
2020-01-20 13:06:51 -0600

    Cambios en Home

--- app/src/main/java/com/example/cheers/homeActivity.java
+++ app/src/main/java/com/example/cheers/homeActivity.java
@@ -3,11 +3,12 @@ package com.example.cheers;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class homeActivity extends AppCompatActivity {
+public class homeActivity extends Activity {
     CardView crearBebida, favoritos;
 
     @Override
